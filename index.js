0b7fae8a50533e86b75cfcaf747cf21960a7668c
Shreshth Sharma official <61823543+shreshth-sharma-official@users.noreply.github.com>
2023-06-24 07:23:16 +0530

    create contact list show on screen

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 const express =require('express');
 const port=8000;
+const path=require('path');
 const app=express();
+
+
+app.set('view engine','ejs');
+// app.set('view',path.join(__dirname,'view'));
+app.set('views', './views');
+
+var contact=[
+    {
+        name:"shreshth",
+        number:"7340578030"
+    },
+    {  
+        name:"dad",
+        number:"9928221785"
+    }
+];
 
 app.get('/',function(req,res){
 
-    res.send('<h1>codeial</h1>');
+    // res.send('<h1>codeial</h1>');
+    return res.render('home',{title:"codeial",contact_list: contact});
 });
+
 
 app.listen(port,function(err){
     if(err)
